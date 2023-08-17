95ce19f2b79cf7b2c54078e8ab1f24b6a083f221
Arsenev Denis <102998182+Arden30@users.noreply.github.com>
2023-08-17 16:59:07 +0300

    Lab6 uploaded

---
+++ Lab6/lab-server/src/main/java/server/client_commands/ExecuteScript.java
@@ -0,0 +1,23 @@
+package server.client_commands;
+
+import common.util_common.Request;
+import common.util_common.Response;
+import server.AbstractCommands.AbstractClientCommand;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Class, which is responsible for executing script from entered file
+ */
+public class ExecuteScript extends AbstractClientCommand {
+
+    public ExecuteScript() {
+        super("execute_script", 1, "read and execute the script from the specified file", "file name");
+    }
+
+    @Override
+    public Response execute(Request request) {
+        return null;
+    }
+}
